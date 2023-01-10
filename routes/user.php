9580c993457c8f9c3cca8de92d860fbbefddc3f9
agussuartawan <80089992+agussuartawan@users.noreply.github.com>
2023-01-10 17:20:58 +0800

    fixing

--- routes/user.php
+++ routes/user.php
@@ -6,8 +6,10 @@ use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth')->group(function () {
-    Route::get('users/get-permission-list', [UserController::class, 'getPermissionList']);
-    Route::resource('users', UserController::class);
-    Route::resource('roles', RoleController::class);
-    Route::resource('permissions', PermissionController::class);
+    Route::controller(UserController::class)->group(function(){
+        Route::get('users/get-permission-list', 'getPermissionList');
+        Route::resource('users');
+        Route::resource('roles');
+        Route::resource('permissions');
+    });
 });
